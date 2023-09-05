029d91e2478f192b50d9138acd4a3553b3dc214d
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2023-09-05 21:38:39 +0530

    made hero section of landing page

---
+++ deon_labs/app/_components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Hero() {
+  return (
+    <div className="bg-hero-pattern-mobile md:bg-hero-pattern h-[50vh] w-[100vw] bg-cover bg-no-repeat">
+    <h1 className="relative top-[13rem] text-center font-tektur text-4xl ">
+      Web3 ecosystem <br/> enabler 
+    </h1>
+    <p className="relative top-[15rem] text-center font-tektur opacity-50 break-normal px-5">
+    Deon Labs is your in-house web3 ecosystem builder driving paradigm shifts into web3 ecosystems
+    </p>
+  </div>
+  )
+}
+
+export default Hero
---
+++ deon_labs/app/page.js
@@ -0,0 +1,14 @@
+import Hero from "./_components/Hero"
+
+
+function page() {
+  return (
+    <main>
+      {/* Hero Section */}
+      <Hero/>
+    </main>
+    
+  )
+}
+
+export default page
