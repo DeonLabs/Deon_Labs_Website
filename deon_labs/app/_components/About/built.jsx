e64e5019a8010d778847f31982df4af81d1e958c
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2023-09-12 03:23:27 +0530

    done

--- deon_labs/app/_components/About/built.jsx
+++ deon_labs/app/_components/About/built.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
+import Image from 'next/image'
 
 function Built() {
   return (
-    <div className='py-[2%]'>
-        <h1 className = "font-tektur text-center text-[1.2rem] leading-[2rem] space-y-[0.5rem] font-semibold md:text-2xl lg:text-4xl">Built by a team from</h1>
-        <div className='flex flex-row align-center justify-between border-4 border-none mx-[2%] py-[5%]'>
-            <img src="./tezoz.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./gari.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./superteam.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./Router.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./bankless.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./dapp_list.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./gitopia.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
-            <img src="./kleros.svg" className='md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]' alt="" />
+    <div className='py-[10%]'>
+        <h1 className = "font-tektur text-center text-[1.2rem] leading-[2rem] space-y-[0.5rem]  md:text-2xl lg:text-4xl">BUILD BY A TEAM FROM</h1>
+        <div className='flex flex-row align-center justify-center gap-x-[4%] md:gap-x-[5%] border-4 border-none mx-[20%] py-[5%] '>
+            <img src="./tezoz.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]' alt="" />
+            <img src="./gari.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]'   alt="" />
+            <img src="./superteam.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]' alt="" />
+            <img src="./Router.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]' alt="" />
+            <img src="./Bankless.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]' alt="" />
+            <img src="./dapp_list.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]'  alt="" />
+            <img src="./gitopia.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]' alt="" />
+            <img src="./kleros.svg" className='h-[2rem] md:h-[4rem] lg:h-[6rem]'  alt="" />
 
         </div>
     </div>
