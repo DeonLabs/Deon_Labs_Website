3c49897a8f74c121b302335937eed5aeaecd4641
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2023-09-12 00:41:32 +0530

    added svg

--- deon_labs/app/_components/Team.jsx
+++ deon_labs/app/_components/Team.jsx
@@ -7,14 +7,14 @@ function Team() {
       Built by a team from
       </h1>
      <div className="flex flex-row align-center justify-between border-4 border-none mx-[2%] py-[5%]">
-      <img src="./tezoz.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./gari.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./superteam.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./router.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./bankless.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./dapp_list.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./gitopia.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
-      <img src="./kleros.png" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./tezoz.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./gari.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./superteam.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./router.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./bankless.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./dapp_list.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./gitopia.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
+      <img src="./kleros.svg" alt class="md:h-[3rem] md:w-[3rem] lg:h-[5rem] lg:w-[5rem]"/>
 
       </div> 
     </div>
