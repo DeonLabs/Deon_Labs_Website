72140ed93ec3b6234006e0b59f11bd6de1b596d4
Rudransh Shinghal <84223519+Ansh1902396@users.noreply.github.com>
2023-09-22 19:45:24 +0530

    LFG

--- deon_labs/app/_components/UI/Record_card.jsx
+++ deon_labs/app/_components/UI/Record_card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Card({ number, type }) {
   return (
-    <div className="mb-[2%] mt-[5%] flex  max-h-[30vh] w-[85vw] flex-col justify-evenly rounded-xl border border-gray-500 bg-black drop-shadow-xl md:mx-[4%] md:h-[15vh] lg:h-[25vh] lg:w-[20vw]">
+    <div className="mb-[2%] mt-[5%] flex  max-h-[30vh] w-[85vw] flex-col justify-evenly rounded-xl border border-gray-500 bg-black drop-shadow-xl md:mx-[4%] md:max-h-screen lg:h-[25vh] lg:w-[20vw]">
       <img src="./Rectangle 60.svg" className="h-[20%] " alt="" />
       <h1 className="font-tektur text-2xl font-semibold ">{number}</h1>
       <h1 className="py-[0.5%] text-2xl font-tektur opacity-50">{type}</h1>
